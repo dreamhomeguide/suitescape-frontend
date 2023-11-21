1d788a779cdec4c9d416a80ce2fcb31fc6400272
melmatx <melpalana13@gmail.com>
2023-11-21 20:30:56 +0800

    Added modal theme for ModalSection component

--- components/ModalSection/ModalSection.js
+++ components/ModalSection/ModalSection.js
@@ -1,23 +1,34 @@
 import { format } from "date-fns";
 import React from "react";
 import { ScrollView, Text } from "react-native";
-import { Modal } from "react-native-paper";
+import { useTheme, Modal } from "react-native-paper";
 
 import style from "./ModalSectionStyles";
 
 const ModalSection = ({ visible, onDismiss, duration, progress }) => {
+  const theme = useTheme();
+
+  const modalTheme = {
+    ...theme,
+    colors: {
+      ...theme.colors,
+      backdrop: "rgba(0,0,0,0.2)",
+    },
+  };
+
   return (
     <Modal
       visible={visible}
       transparent
       onDismiss={onDismiss}
       contentContainerStyle={style.mainContainer}
+      theme={modalTheme}
     >
       <Text style={style.headerText}>Sections</Text>
       <ScrollView horizontal contentContainerStyle={style.scrollViewContainer}>
         <Text>
-          {progress ? format(progress, "m:ss") : null}/
-          {duration ? format(duration, "mm:ss") : null}
+          {progress ? format(progress, "m:ss") : "-:--"}/
+          {duration ? format(duration, "mm:ss") : "--:--"}
         </Text>
       </ScrollView>
     </Modal>
