1323e809d74ade5174edd2ad3bbd9467146e9caf
melmatx <melpalana13@gmail.com>
2023-11-21 20:29:29 +0800

    Added mappings for booking data and added mutation for validation

--- screens/GuestInfo/GuestInfo.js
+++ screens/GuestInfo/GuestInfo.js
@@ -1,3 +1,4 @@
+import { useMutation } from "@tanstack/react-query";
 import React, { useEffect, useRef, useState } from "react";
 import {
   KeyboardAvoidingView,
@@ -18,21 +19,36 @@ import { useBookingContext } from "../../contexts/BookingContext";
 import regionsList from "../../data/regionsData";
 import useFetchAPI from "../../hooks/useFetchAPI";
 import { Routes } from "../../navigation/Routes";
+import SuitescapeAPI from "../../services/SuitescapeAPI";
+import { handleApiError, handleApiResponse } from "../../utilities/apiHelpers";
 
 const genderList = [
   {
     label: "Male",
-    value: "Male",
+    value: "male",
   },
   {
     label: "Female",
-    value: "Female",
+    value: "female",
   },
   {
     label: "Other",
-    value: "Other",
+    value: "other",
   },
 ];
+
+const mappings = {
+  firstName: "firstname",
+  lastName: "lastname",
+  email: "email",
+  gender: "gender",
+  address: "address",
+  zipCode: "zipcode",
+  city: "city",
+  region: "region",
+  mobileNumber: "mobile_number",
+  message: "message",
+};
 
 const GuestInfo = ({ navigation }) => {
   const { bookingState, setBookingData } = useBookingContext();
@@ -51,6 +67,7 @@ const GuestInfo = ({ navigation }) => {
 
   const [showGenderDropDown, setShowGenderDropDown] = useState(false);
   const [showRegionDropDown, setShowRegionDropDown] = useState(false);
+  const [errors, setErrors] = useState(null);
 
   const { data: userData, isLoading, abort } = useFetchAPI("/user");
 
@@ -58,21 +75,54 @@ const GuestInfo = ({ navigation }) => {
 
   useEffect(() => {
     if (userData) {
-      // stateKey: userDataKey
-      const mappings = {
-        firstName: "firstname",
-        lastName: "lastname",
-        email: "email",
-        mobileNumber: "mobile_number",
-      };
-
-      for (const [stateKey, userDataKey] of Object.entries(mappings)) {
-        if (!bookingState[stateKey]) {
-          setBookingData({ [stateKey]: userData[userDataKey] });
+      Object.entries(mappings).forEach(([state, userDataKey]) => {
+        if (userData[userDataKey] && !bookingState[state]) {
+          setBookingData({ [state]: userData[userDataKey] });
         }
-      }
+      });
     }
   }, [userData]);
+
+  const validateMutation = useMutation({
+    mutationFn: ({ guestData }) =>
+      SuitescapeAPI.post("/bookings/validate-info", guestData),
+    onSuccess: (response) =>
+      handleApiResponse({
+        response,
+        onError: (e) => {
+          setErrors(e.errors);
+        },
+        onSuccess: () => {
+          navigation.navigate(Routes.BOOKING_SUMMARY);
+        },
+      }),
+    onError: (err) =>
+      handleApiError({
+        error: err,
+        handleErrors: (e) => {
+          setErrors(e.errors);
+        },
+      }),
+  });
+
+  const validateInfo = async () => {
+    // Convert bookingState to backend format
+    const guestData = Object.entries(mappings).reduce(
+      (acc, [state, userDataKey]) => {
+        acc[userDataKey] = bookingState[state];
+        return acc;
+      },
+      {}, // acc - initial value
+    );
+
+    validateMutation.mutate({ guestData });
+  };
+
+  const clearErrorWhenNotEmpty = (value, key) => {
+    if (value) {
+      setErrors((prevErrors) => ({ ...prevErrors, [key]: "" }));
+    }
+  };
 
   return (
     <>
@@ -93,42 +143,49 @@ const GuestInfo = ({ navigation }) => {
           </View>
           <FormInput
             value={firstName}
-            onChangeText={(text) => {
-              setBookingData({ firstName: text });
+            onChangeText={(value) => {
+              setBookingData({ firstName: value });
+              clearErrorWhenNotEmpty(value, mappings.firstName);
             }}
             placeholder="First Name"
             textContentType="givenName"
             autoCapitalize="words"
             autoCorrect={false}
             returnKeyType="next"
+            errorMessage={errors?.firstname}
           />
           <FormInput
             value={lastName}
-            onChangeText={(text) => {
-              setBookingData({ lastName: text });
+            onChangeText={(value) => {
+              setBookingData({ lastName: value });
+              clearErrorWhenNotEmpty(value, mappings.lastName);
             }}
             placeholder="Last Name"
             textContentType="familyName"
             autoCapitalize="words"
             autoCorrect={false}
             returnKeyType="next"
+            errorMessage={errors?.lastname}
           />
           <FormInput
             type="dropdown"
             value={gender}
             setValue={(value) => {
               setBookingData({ gender: value });
+              clearErrorWhenNotEmpty(value, mappings.gender);
             }}
             label="Gender"
             visible={showGenderDropDown}
             onDismiss={() => setShowGenderDropDown(false)}
             showDropDown={() => setShowGenderDropDown(true)}
             list={genderList}
+            errorMessage={errors?.gender}
           />
           <FormInput
             value={email}
-            onChangeText={(text) => {
-              setBookingData({ email: text });
+            onChangeText={(value) => {
+              setBookingData({ email: value });
+              clearErrorWhenNotEmpty(value, mappings.email);
             }}
             placeholder="Email Address"
             keyboardType="email-address"
@@ -136,68 +193,81 @@ const GuestInfo = ({ navigation }) => {
             autoCapitalize="none"
             autoCorrect={false}
             returnKeyType="next"
+            errorMessage={errors?.email}
           />
           <FormInput
             value={address}
-            onChangeText={(text) => {
-              setBookingData({ address: text });
+            onChangeText={(value) => {
+              setBookingData({ address: value });
+              clearErrorWhenNotEmpty(value, mappings.address);
             }}
+            maxLength={40}
             placeholder="Address"
             textContentType="fullStreetAddress"
             autoCapitalize="words"
             autoCorrect={false}
             returnKeyType="next"
+            errorMessage={errors?.address}
           />
           <FormInput
             value={zipCode}
-            onChangeText={(text) => {
-              setBookingData({ zipCode: text });
+            onChangeText={(value) => {
+              setBookingData({ zipCode: value });
+              clearErrorWhenNotEmpty(value, mappings.zipCode);
             }}
             placeholder="Zip/Post Code"
             keyboardType="number-pad"
             textContentType="postalCode"
             autoCorrect={false}
             returnKeyType="next"
+            errorMessage={errors?.zipcode}
           />
           <FormInput
             value={city}
-            onChangeText={(text) => {
-              setBookingData({ city: text });
+            onChangeText={(value) => {
+              setBookingData({ city: value });
+              clearErrorWhenNotEmpty(value, mappings.city);
             }}
             placeholder="City"
             textContentType="addressCity"
             autoCapitalize="words"
             autoCorrect={false}
             returnKeyType="next"
+            errorMessage={errors?.city}
           />
           <FormInput
             type="dropdown"
             value={region}
             setValue={(value) => {
               setBookingData({ region: value });
+              clearErrorWhenNotEmpty(value, mappings.region);
             }}
             label="Region"
             visible={showRegionDropDown}
             onDismiss={() => setShowRegionDropDown(false)}
             showDropDown={() => setShowRegionDropDown(true)}
             list={regionsList}
+            errorMessage={errors?.region}
           />
           <FormInput
             value={mobileNumber}
-            onChangeText={(text) => {
-              setBookingData({ mobileNumber: text });
+            onChangeText={(value) => {
+              setBookingData({ mobileNumber: value });
+              clearErrorWhenNotEmpty(value, mappings.mobileNumber);
             }}
             placeholder="Mobile Number"
             keyboardType="phone-pad"
             textContentType="telephoneNumber"
             autoCorrect={false}
             returnKeyType="next"
+            errorMessage={errors?.mobile_number}
           />
           <FormInput
             type="textarea"
             value={message}
-            onChangeText={(text) => {
-              setBookingData({ message: text });
+            onChangeText={(value) => {
+              setBookingData({ message: value });
+              clearErrorWhenNotEmpty(value, mappings.message);
             }}
             label="Message (Optional)"
             blurOnSubmit
@@ -205,15 +275,12 @@ const GuestInfo = ({ navigation }) => {
             onFocus={() =>
               setTimeout(() => scrollViewRef.current.scrollToEnd(), 300)
             }
+            errorMessage={errors?.message}
           />
         </ScrollView>
       </KeyboardAvoidingView>
       <AppFooter>
-        <ButtonLarge
-          onPress={() => navigation.navigate(Routes.BOOKING_SUMMARY)}
-        >
-          Confirm
-        </ButtonLarge>
+        <ButtonLarge onPress={() => validateInfo()}>Confirm</ButtonLarge>
       </AppFooter>
       <LoadingDialog visible={isLoading} onCancel={() => abort()} />
     </>
