32f303d3c04e22a2c38613f16e7c1063202d7a0b
melmatx <melpalana13@gmail.com>
2023-11-21 20:30:23 +0800

    Removed cart and filter in Home Screen and replace with only search button

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,56 +1,36 @@
-import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
-import Ionicons from "@expo/vector-icons/Ionicons";
+import Entypo from "@expo/vector-icons/Entypo";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 import React from "react";
 import { Pressable, RefreshControl, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import style from "./HomeStyles";
 import globalStyles, { pressedOpacity } from "../../assets/styles/globalStyles";
-import HeaderIcon from "../../components/HeaderIcon/HeaderIcon";
-import IconBadge from "../../components/IconBadge/IconBadge";
+import HeaderIconView from "../../components/HeaderIconView/HeaderIconView";
 import VideoFeed from "../../components/VideoFeed/VideoFeed";
 import useFetchVideos from "../../hooks/useFetchVideos";
 
 const Home = () => {
   const { videos, fetchNextPage, isRefreshing, refresh } = useFetchVideos();
+
   const bottomTabHeight = useBottomTabBarHeight();
+  const insets = useSafeAreaInsets();
 
   return (
     <View style={style.mainContainer}>
-      <HeaderIcon>
+      <HeaderIconView right>
         <Pressable
-          onPress={() => console.log("Options")}
+          onPress={() => console.log("Search")}
           style={({ pressed }) => pressedOpacity(pressed)}
         >
-          <Ionicons
-            name="options"
+          <Entypo
+            name="magnifying-glass"
             size={30}
             color="white"
             style={globalStyles.iconShadow}
           />
         </Pressable>
-      </HeaderIcon>
-      <HeaderIcon right>
-        <Pressable
-          onPress={() => console.log("Cart")}
-          style={({ pressed }) => pressedOpacity(pressed)}
-        >
-          <IconBadge count={0}>
-            <FontAwesome5
-              name="shopping-cart"
-              size={22}
-              color="white"
-              style={globalStyles.iconShadow}
-            />
-          </IconBadge>
-        </Pressable>
-
-        {/*<NotificationBadge*/}
-        {/*  size={0}*/}
-        {/*  iconStyle={globalStyles.iconShadow}*/}
-        {/*  onPress={() => console.log("Notifications")}*/}
-        {/*/>*/}
-      </HeaderIcon>
+      </HeaderIconView>
       <VideoFeed
         videos={videos}
         onEndReached={() => fetchNextPage()}
@@ -59,6 +39,7 @@ const Home = () => {
             tintColor="white"
             refreshing={isRefreshing}
             onRefresh={() => refresh()}
+            progressViewOffset={insets.top}
           />
         }
         bottomTabHeight={bottomTabHeight}
