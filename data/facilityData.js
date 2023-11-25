8ba55dad527b741c472f1f25279d32e6e2ab5cd2
melmatx <melpalana13@gmail.com>
2023-11-25 14:19:28 +0800

    Created facility data to list all facilities in Search screen

---
+++ data/facilityData.js
@@ -0,0 +1,11 @@
+export default [
+  { label: "House", value: "house" },
+  { label: "Hotel", value: "hotel" },
+  { label: "Apartment", value: "apartment" },
+  { label: "Condominium", value: "condominium" },
+  { label: "Villas", value: "villas" },
+  { label: "Cabin", value: "cabin" },
+  { label: "Hostel", value: "hostel" },
+  { label: "Lodges", value: "lodges" },
+  { label: "Resort", value: "resort" },
+];
