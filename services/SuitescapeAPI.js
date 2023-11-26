477d7e6832ef5edc2e2bf497640b0a8542039d43
melmatx <melpalana13@gmail.com>
2023-11-26 17:12:17 +0800

    Changed to Hostinger server

--- services/SuitescapeAPI.js
+++ services/SuitescapeAPI.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 // Replace this with your local/private IP Address
-const serverIpAddress = "192.168.100.151";
+// const serverIpAddress = "192.168.100.151";
+//
+// export const baseURL = `http://${serverIpAddress}/suitescape-api.test/api`;
 
-export const baseURL = `http://${serverIpAddress}/suitescape-api.test/api`;
-
-// export const baseURL = `https://suitescape.dream-homeseller.com/api`;
+export const baseURL = `https://suitescape.dream-homeseller.com/api`;
 
 const SuitescapeAPI = axios.create({
   baseURL,
