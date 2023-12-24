daa41abcc0d8d7557a3480949677ec532c0abbb4
melmatx <melpalana13@gmail.com>
2023-12-24 18:54:48 +0800

    Added share sheet and navigation to message screen and host profile

--- components/DetailsHostView/DetailsHostView.js
+++ components/DetailsHostView/DetailsHostView.js
@@ -1,21 +1,41 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { Text, View } from "react-native";
+import { Pressable, Share, Text, View } from "react-native";
 
 import style from "./DetailsHostViewStyles";
+import { Colors } from "../../assets/Colors";
 import detailsStyles from "../../assets/styles/detailsStyles";
+import { pressedOpacity } from "../../assets/styles/globalStyles";
+import { useListingContext } from "../../contexts/ListingContext";
+import { Routes } from "../../navigation/Routes";
+import { baseURL } from "../../services/SuitescapeAPI";
 import ButtonLink from "../ButtonLink/ButtonLink";
 import ButtonSocialActionsView from "../ButtonSocialActionsView/ButtonSocialActionsView";
 import ProfileImage from "../ProfileImage/ProfileImage";
 
 const DetailsHostView = ({ hostName }) => {
+  const navigation = useNavigation();
+  const { listing } = useListingContext();
+
+  const handleHostPress = () => {
+    navigation.navigate(Routes.PROFILE_HOST, {
+      hostId: listing.host.id,
+    });
+  };
+
   return (
     <View style={detailsStyles.plainContainer}>
       <View style={style.hostContentContainer}>
-        <ProfileImage />
+        <Pressable
+          onPress={handleHostPress}
+          style={({ pressed }) => pressedOpacity(pressed)}
+        >
+          <ProfileImage size={60} borderWidth={1} borderColor={Colors.blue} />
+        </Pressable>
 
         <View style={style.hostDetailsContainer}>
           {/* Host Name */}
-          <ButtonLink textStyle={style.hostNameText}>
+          <ButtonLink onPress={handleHostPress} textStyle={style.hostNameText}>
             {hostName ?? "Loading..."}
           </ButtonLink>
 
@@ -30,7 +50,15 @@ const DetailsHostView = ({ hostName }) => {
       </View>
 
       {/* Listing Actions */}
-      <ButtonSocialActionsView />
+      <ButtonSocialActionsView
+        onChatNow={() => navigation.navigate(Routes.CHAT)}
+        onShare={async () =>
+          await Share.share({
+            message:
+              "Check out this listing: " + baseURL + "/listings/" + listing.id,
+          })
+        }
+      />
     </View>
   );
 };
