effed692045773d3e0be5627724ded8ad5436661
melmatx <melpalana13@gmail.com>
2023-12-24 18:50:30 +0800

    Added authorization check in Cart.js

--- screens/Cart/Cart.js
+++ screens/Cart/Cart.js
@@ -1,6 +1,20 @@
 import React from "react";
+import { Text, View } from "react-native";
+
+import globalStyles from "../../assets/styles/globalStyles";
+import { getHeaderToken } from "../../services/SuitescapeAPI";
 
 const Cart = () => {
+  const token = getHeaderToken();
+
+  if (!token) {
+    return (
+      <View style={globalStyles.flexCenter}>
+        <Text>Not logged in</Text>
+      </View>
+    );
+  }
+
   return null;
 };
 
