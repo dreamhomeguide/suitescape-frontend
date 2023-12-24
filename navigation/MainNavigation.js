0cda5f066a0b3d94f1adb7dbd2d73fd7e4f37cd0
melmatx <melpalana13@gmail.com>
2023-12-24 19:20:54 +0800

    Added new screens and fixed slide right animation for ios

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -10,17 +10,23 @@ import ButtonBack from "../components/ButtonBack/ButtonBack";
 import { useAuth } from "../contexts/AuthContext";
 import { useSettings } from "../contexts/SettingsContext";
 import BookingSummary from "../screens/BookingSummary/BookingSummary";
+import ChangePassword from "../screens/ChangePassword/ChangePassword";
+import Chat from "../screens/Chat/Chat";
 import CheckAvailability from "../screens/CheckAvailability/CheckAvailability";
 import Feedback from "../screens/Feedback/Feedback";
 import Filter from "../screens/Filter/Filter";
 import GuestInfo from "../screens/GuestInfo/GuestInfo";
+import Liked from "../screens/Liked/Liked";
 import ListingDetails from "../screens/ListingDetails/ListingDetails";
+import ListingRatings from "../screens/ListingRatings/ListingRatings";
 import Login from "../screens/Login/Login";
 import ManageAccount from "../screens/ManageAccount/ManageAccount";
+import MessagesSearch from "../screens/MessagesSearch/MessagesSearch";
 import Onboarding from "../screens/Onboarding/Onboarding";
 import PaymentMethod from "../screens/PaymentMethod/PaymentMethod";
 import ProfileHost from "../screens/ProfileHost/ProfileHost";
 import RoomDetails from "../screens/RoomDetails/RoomDetails";
+import Saved from "../screens/Saved/Saved";
 import Search from "../screens/Search/Search";
 import SelectDates from "../screens/SelectDates/SelectDates";
 import SignUp from "../screens/SignUp/SignUp";
@@ -34,13 +40,16 @@ const MainNavigation = () => {
   const theme = useTheme();
   const colorScheme = useColorScheme();
 
+  const slideRightInputAnim =
+    Platform.OS === "ios" ? "simple_push" : "slide_from_right";
+
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       {settings.onboardingEnabled && (
         <Stack.Screen name={Routes.ONBOARDING} component={Onboarding} />
       )}
 
-      {authState.userToken ? (
+      {authState.userToken || settings.guestModeEnabled ? (
         <Stack.Screen name={Routes.BOTTOM_TABS} component={BottomTabs} />
       ) : (
         <Stack.Group screenOptions={{ animation: "slide_from_bottom" }}>
@@ -48,6 +57,15 @@ const MainNavigation = () => {
           <Stack.Screen name={Routes.SIGNUP} component={SignUp} />
         </Stack.Group>
       )}
+
+      <Stack.Screen
+        name={Routes.CHAT}
+        component={Chat}
+        options={{
+          animation: slideRightInputAnim,
+          animationDuration: 150,
+        }}
+      />
 
       <Stack.Group
         screenOptions={({ navigation }) => ({
@@ -59,7 +77,7 @@ const MainNavigation = () => {
             Platform.OS === "android"
               ? ({ canGoBack, tintColor }) =>
                   canGoBack && (
-                    <View style={{ marginLeft: -16 }}>
+                    <View style={{ marginLeft: -12, marginRight: 5 }}>
                       <ButtonBack
                         onPress={() => navigation.goBack()}
                         color={tintColor}
@@ -77,8 +95,19 @@ const MainNavigation = () => {
         <Stack.Screen
           name={Routes.SEARCH}
           component={Search}
-          options={{ title: "Search Destination" }}
+          options={{
+            title: "Search Destination",
+            animation: slideRightInputAnim,
+            animationDuration: 250,
+          }}
         />
+
+        <Stack.Screen
+          name={Routes.SEARCH_MESSAGES}
+          component={MessagesSearch}
+          options={{ animation: "fade" }}
+        />
+
         <Stack.Screen
           name={Routes.SELECT_DATES}
           component={SelectDates}
@@ -87,6 +116,7 @@ const MainNavigation = () => {
             gestureEnabled: false,
           }}
         />
+
         <Stack.Screen
           name={Routes.FEEDBACK}
           component={Feedback}
@@ -97,13 +127,35 @@ const MainNavigation = () => {
             animation: "fade_from_bottom",
           }}
         />
+
         <Stack.Screen name={Routes.MANAGE_ACCOUNT} component={ManageAccount} />
+        <Stack.Screen
+          name={Routes.CHANGE_PASSWORD}
+          component={ChangePassword}
+        />
+        <Stack.Screen name={Routes.SAVED} component={Saved} />
+        <Stack.Screen name={Routes.LIKED} component={Liked} />
 
         <Stack.Group
           screenOptions={{
             animation: "slide_from_right",
+            contentStyle: {
+              backgroundColor: "white",
+            },
           }}
         >
+          <Stack.Screen name={Routes.FILTER} component={Filter} />
+          <Stack.Screen name={Routes.GUEST_INFO} component={GuestInfo} />
+          <Stack.Screen
+            name={Routes.PAYMENT_METHOD}
+            component={PaymentMethod}
+            options={{ title: "Payment" }}
+          />
+          <Stack.Screen
+            name={Routes.LISTING_RATINGS}
+            component={ListingRatings}
+          />
+
           <Stack.Group
             screenOptions={{
               contentStyle: {
@@ -128,22 +180,6 @@ const MainNavigation = () => {
               component={BookingSummary}
             />
             <Stack.Screen name={Routes.PROFILE_HOST} component={ProfileHost} />
-          </Stack.Group>
-
-          <Stack.Group
-            screenOptions={{
-              contentStyle: {
-                backgroundColor: "white",
-              },
-            }}
-          >
-            <Stack.Screen name={Routes.GUEST_INFO} component={GuestInfo} />
-            <Stack.Screen
-              name={Routes.PAYMENT_METHOD}
-              component={PaymentMethod}
-              options={{ title: "Payment" }}
-            />
-            <Stack.Screen name={Routes.FILTER} component={Filter} />
           </Stack.Group>
         </Stack.Group>
       </Stack.Group>
