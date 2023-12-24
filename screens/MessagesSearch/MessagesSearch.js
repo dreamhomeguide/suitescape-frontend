71124bb8cd76b693bce610ce6905195b5ce50b75
melmatx <melpalana13@gmail.com>
2023-12-24 19:28:52 +0800

    Used FormInput component for MessagesSearch.js

--- screens/MessagesSearch/MessagesSearch.js
+++ screens/MessagesSearch/MessagesSearch.js
@@ -1,16 +1,17 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
 import React from "react";
-import { TextInput, View } from "react-native";
+import { View } from "react-native";
 
+import FormInput from "../../components/FormInput/FormInput";
 import style from "../Messages/MessagesStyle";
 const MessagesSearch = () => {
   return (
-    <View style={{ paddingHorizontal: 15 }}>
+    <View>
       <View style={style.searchContainer}>
-        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-          <TextInput autoFocus style={style.searchText} />
-          <FontAwesome name="search" size={20} color="black" />
-        </View>
+        {/*<View style={{ flexDirection: "row", justifyContent: "space-between" }}>*/}
+        {/*  <TextInput autoFocus style={style.searchText} />*/}
+        {/*  <FontAwesome name="search" size={20} color="black" />*/}
+        {/*</View>*/}
+        <FormInput autoFocus placeholder="Search" />
       </View>
     </View>
   );
