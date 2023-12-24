4d501fb89d3db80e53fa4e76e3943684a5bd1c8e
melmatx <melpalana13@gmail.com>
2023-12-24 19:40:25 +0800

    Changed destructuring for host data and optimized Flatlists for Host Listings and Reviews

--- screens/ProfileHost/ProfileHost.js
+++ screens/ProfileHost/ProfileHost.js
@@ -1,6 +1,6 @@
 import { useTheme } from "@react-navigation/native";
-import React, { useLayoutEffect } from "react";
-import { Text, View } from "react-native";
+import React, { useCallback, useLayoutEffect } from "react";
+import { View } from "react-native";
 import { Tabs } from "react-native-collapsible-tab-view";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
@@ -8,22 +8,23 @@ import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import globalStyles from "../../assets/styles/globalStyles";
 import HeaderProfileHost from "../../components/HeaderProfileHost/HeaderProfileHost";
 import ListingItem from "../../components/ListingItem/ListingItem";
+import ReviewItem from "../../components/ReviewItem/ReviewItem";
 import useFetchAPI from "../../hooks/useFetchAPI";
-import { IoniconsHeaderButton } from "../../navigation/HeaderButtons";
+import { FontelloHeaderButton } from "../../navigation/HeaderButtons";
 import { TabBar } from "../../navigation/TopTabs/TopTabs";
 
 const ProfileHost = ({ navigation, route }) => {
-  const listingId = route.params.listingId;
+  const hostId = route.params.hostId;
 
-  const { data: host } = useFetchAPI(`/listings/${listingId}/host`);
+  const { data: host } = useFetchAPI(`/hosts/${hostId}`);
 
   // Destructure hostData
   const {
     listings,
-    all_reviews: allReviews,
+    reviews,
     listings_count: listingsCount,
-    total_likes_count: totalLikesCount,
-    total_reviews_count: totalReviewsCount,
+    listings_likes_count: likesCount,
+    listings_reviews_count: reviewsCount,
     ...hostData
   } = host || {};
 
@@ -33,31 +34,42 @@ const ProfileHost = ({ navigation, route }) => {
   useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => (
-        <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+        <HeaderButtons HeaderButtonComponent={FontelloHeaderButton}>
           <Item
             title="menu"
-            iconName="menu"
+            iconName="hamburger-regular"
             color={colors.text}
-            onPress={() => console.log("menu")}
+            onPress={() => console.log("Menu pressed")}
           />
         </HeaderButtons>
       ),
     });
   }, [navigation]);
 
+  const renderListingItem = useCallback(
+    ({ item }) => <ListingItem item={item} />,
+    [],
+  );
+
+  const renderReviewItem = useCallback(
+    ({ item }) => <ReviewItem item={item} />,
+    [],
+  );
+
   return (
     <View style={globalStyles.flexFull}>
       <Tabs.Container
+        lazy
         renderHeader={() => (
           <HeaderProfileHost
             hostName={hostData.fullname}
             userName={
               // Temporary username for host
-              "@" + (host ? hostData.firstname + hostData.lastname : "")
+              hostData.firstname + hostData.lastname
             }
             listingsCount={listingsCount}
-            likesCount={totalLikesCount}
-            reviewsCount={totalReviewsCount}
+            likesCount={likesCount}
+            reviewsCount={reviewsCount}
           />
         )}
         renderTabBar={(props) => <TabBar defaultProps={props} />}
@@ -65,29 +77,27 @@ const ProfileHost = ({ navigation, route }) => {
         <Tabs.Tab name="Listings">
           <Tabs.FlatList
             data={listings}
-            contentContainerStyle={{ paddingBottom: insets.bottom }}
+            contentContainerStyle={{ paddingBottom: insets.bottom + 15 }}
             keyExtractor={(item) => item.id.toString()}
-            renderItem={({ item }) => <ListingItem item={item} />}
+            renderItem={renderListingItem}
+            initialNumToRender={10}
+            windowSize={5}
+            maxToRenderPerBatch={5}
+            updateCellsBatchingPeriod={30}
+            removeClippedSubviews={false}
           />
         </Tabs.Tab>
         <Tabs.Tab name="Reviews">
           <Tabs.FlatList
-            data={allReviews}
-            contentContainerStyle={{ paddingBottom: insets.bottom }}
+            data={reviews}
+            contentContainerStyle={{ paddingBottom: insets.bottom + 15 }}
             keyExtractor={(item) => item.id.toString()}
-            renderItem={({ item }) => (
-              <View
-                style={{
-                  marginTop: 15,
-                  margin: 10,
-                  padding: 10,
-                  backgroundColor: "white",
-                  borderRadius: 10,
-                }}
-              >
-                <Text>{item.content}</Text>
-              </View>
-            )}
+            renderItem={renderReviewItem}
+            initialNumToRender={10}
+            windowSize={5}
+            maxToRenderPerBatch={5}
+            updateCellsBatchingPeriod={30}
+            removeClippedSubviews={false}
           />
         </Tabs.Tab>
       </Tabs.Container>
