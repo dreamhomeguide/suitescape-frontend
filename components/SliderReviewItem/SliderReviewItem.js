21745e465b1c8072bb152948991c14489017b732
melmatx <melpalana13@gmail.com>
2023-12-24 19:51:08 +0800

    Removed pressable for whole component and instead only on user's name (since it is already on the listing)

--- components/SliderReviewItem/SliderReviewItem.js
+++ components/SliderReviewItem/SliderReviewItem.js
@@ -1,32 +1,36 @@
 import React, { memo } from "react";
-import { Text, View } from "react-native";
-import { RectButton } from "react-native-gesture-handler";
+import { Pressable, Text, View } from "react-native";
 
 import style from "./SliderReviewItemStyles";
+import { pressedOpacity } from "../../assets/styles/globalStyles";
 import ProfileImage from "../ProfileImage/ProfileImage";
 import StarRatingView from "../StarRatingView/StarRatingView";
 
-const SliderReviewItem = ({ item, onPress, itemWidth, itemMargin }) => {
-  return (
-    <RectButton
-      onPress={onPress}
-      style={style.mainContainer({ itemWidth, itemMargin })}
-    >
-      <View style={style.userContainer}>
-        {/*<Avatar.Image source={item.img} size={50} style={style.avatar} />*/}
+const SliderReviewItem = ({ item, itemWidth, itemMargin }) => {
+  const { user, rating, content } = item;
 
+  return (
+    <View style={style.mainContainer({ itemWidth, itemMargin })}>
+      <Pressable
+        onPress={() => console.log("User ID: ", user.id)}
+        style={({ pressed }) => ({
+          ...style.userContainer,
+          ...pressedOpacity(pressed),
+        })}
+      >
         <ProfileImage size={45} />
         <View style={style.userNameContainer}>
           <Text style={{ ...style.userName, ...style.text }} numberOfLines={1}>
-            {item.user.fullname}
+            {user.fullname}
           </Text>
-          <StarRatingView starSize={15} rating={item.rating} />
+          <StarRatingView starSize={15} rating={rating} />
         </View>
-      </View>
+      </Pressable>
+
       <View style={style.reviewContainer}>
-        <Text style={style.text}>{item.content}</Text>
+        <Text style={style.text}>{content}</Text>
       </View>
-    </RectButton>
+    </View>
   );
 };
 
