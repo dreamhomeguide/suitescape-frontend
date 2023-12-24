ce48d13538e90c66ddaf68b91e193a95ecd4fa19
melmatx <melpalana13@gmail.com>
2023-12-24 18:49:51 +0800

    Added Fontello back icon and improve onPress function

--- components/ButtonBack/ButtonBack.js
+++ components/ButtonBack/ButtonBack.js
@@ -1,23 +1,36 @@
-import Ionicons from "@expo/vector-icons/Ionicons";
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { Pressable, View } from "react-native";
 
 import style from "./ButtonBackStyles";
+import Fontello from "../../assets/fontello/Fontello";
 import { pressedOpacity } from "../../assets/styles/globalStyles";
 
-const ButtonBack = ({ onPress, color = "black" }) => (
-  <Pressable onPress={onPress}>
-    {({ pressed }) => (
-      <View
-        style={{
-          ...style.backButton,
-          ...pressedOpacity(pressed, 0.7),
-        }}
-      >
-        <Ionicons name="chevron-back" size={30} color={color} />
-      </View>
-    )}
-  </Pressable>
-);
+const ButtonBack = ({ onPress, color = "black" }) => {
+  const navigation = useNavigation();
+
+  return (
+    <Pressable
+      onPress={() => {
+        if (onPress) {
+          onPress();
+        } else {
+          navigation.goBack();
+        }
+      }}
+    >
+      {({ pressed }) => (
+        <View
+          style={{
+            ...style.backButton,
+            ...pressedOpacity(pressed, 0.6),
+          }}
+        >
+          <Fontello name="chevron-left-regular" size={20} color={color} />
+        </View>
+      )}
+    </Pressable>
+  );
+};
 
 export default ButtonBack;
