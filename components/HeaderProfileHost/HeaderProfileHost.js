2846de82a45a9a727fc3edbb4d5373325720f330
melmatx <melpalana13@gmail.com>
2023-12-24 19:10:40 +0800

    Added navigation to message screen and placeholder for username in HeaderProfileHost.js

--- components/HeaderProfileHost/HeaderProfileHost.js
+++ components/HeaderProfileHost/HeaderProfileHost.js
@@ -1,9 +1,11 @@
+import { useNavigation } from "@react-navigation/native";
 import { Image } from "expo-image";
 import React from "react";
 import { Text, useWindowDimensions, View } from "react-native";
 
 import style from "./HeaderProfileHostStyles";
 import globalStyles from "../../assets/styles/globalStyles";
+import { Routes } from "../../navigation/Routes";
 import Button from "../Button/Button";
 import ProfileImage from "../ProfileImage/ProfileImage";
 
@@ -17,6 +19,7 @@ const HeaderProfileHost = ({
   reviewsCount,
 }) => {
   const { height } = useWindowDimensions();
+  const navigation = useNavigation();
 
   const heightAdjustment = height > MIN_HEIGHT ? 45 : -10;
   const coverHeight = height / 4 - heightAdjustment;
@@ -26,7 +29,6 @@ const HeaderProfileHost = ({
       <Image
         source={require("../../assets/images/onboarding/page1.png")}
         style={{ height: coverHeight, ...style.coverImageContainer }}
-        pointerEvents="none"
       />
       <View style={style.mainContentContainer} pointerEvents="box-none">
         <ProfileImage
@@ -38,8 +40,10 @@ const HeaderProfileHost = ({
         />
         <View style={style.contentContainer} pointerEvents="box-none">
           <View style={style.nameContainer} pointerEvents="none">
-            <Text style={style.hostName}>{hostName}</Text>
-            <Text style={style.userName}>{userName}</Text>
+            <Text style={style.hostName}>{hostName ?? "Loading..."}</Text>
+            <Text style={style.userName}>
+              {"@" + (!userName ? "" : userName.replaceAll(" ", ""))}
+            </Text>
           </View>
           <View style={style.overviewContainer} pointerEvents="none">
             <View style={style.overviewItemContainer}>
@@ -58,8 +62,17 @@ const HeaderProfileHost = ({
             </View>
           </View>
           <View style={style.actionButtonsContainer} pointerEvents="box-none">
-            <Button containerStyle={style.buttonContainer}>Message</Button>
-            <Button containerStyle={style.buttonContainer} outlined>
+            <Button
+              onPress={() => navigation.navigate(Routes.CHAT)}
+              containerStyle={style.buttonContainer}
+            >
+              Message
+            </Button>
+            <Button
+              onPress={() => console.log("Host liked")}
+              containerStyle={style.buttonContainer}
+              outlined
+            >
               Like
             </Button>
           </View>
