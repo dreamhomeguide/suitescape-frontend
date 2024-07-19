b63117fd5c7fc4276f75ec2df7624ce6cf1f6fbe
melmatx <melpalana13@gmail.com>
2024-07-19 15:45:09 +0800

    Added setting badge count when unread count changes

--- hooks/useUnreadCount.js
+++ hooks/useUnreadCount.js
@@ -1,4 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
+import * as Notifications from "expo-notifications";
+import { useEffect } from "react";
 
 import { useSettings } from "../contexts/SettingsContext";
 import { fetchAllChats } from "../services/apiService";
@@ -14,6 +16,30 @@ const useUnreadCount = () => {
       return data?.filter((chat) => chat.unread_messages_count > 0).length;
     },
   });
+
+  useEffect(() => {
+    (async () => {
+      await Notifications.setBadgeCountAsync(unreadCount);
+      console.log("Updated badge count:", unreadCount);
+    })();
+  }, [unreadCount]);
+
+  // Clear badge count when app is in foreground
+  // useEffect(() => {
+  //   const subscription = AppState.addEventListener(
+  //     "change",
+  //     async (nextAppState) => {
+  //       if (nextAppState === "active") {
+  //         await Notifications.setBadgeCountAsync(0);
+  //         console.log("Cleared badge count!");
+  //       }
+  //     },
+  //   );
+  //
+  //   return () => {
+  //     subscription.remove();
+  //   };
+  // }, []);
 
   return unreadCount;
 };
